49885fbf511f7b06a3cece5eab9b86492f35f434
Maksim Maltsau <mmaltsau@edmunds.com>
2021-11-04 19:00:47 +0300

    add dockerbuild for tomcat application

--- rest-client-core/src/main/java/maltsau/maksim/bff/rest/client/core/config/RestClientSpringConfig.java
+++ rest-client-core/src/main/java/maltsau/maksim/bff/rest/client/core/config/RestClientSpringConfig.java
@@ -1,11 +1,31 @@
 package maltsau.maksim.bff.rest.client.core.config;
 
+import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
 
 @Configuration
 @ComponentScan("maltsau.maksim.bff.rest.client.core")
 @EnableAsync
-public class RestClientSpringConfig {
+public class RestClientSpringConfig implements AsyncConfigurer {
+    @Override
+    public Executor getAsyncExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(50);
+        executor.setMaxPoolSize(200);
+        executor.setQueueCapacity(200);
+        executor.setThreadNamePrefix("bff-custom-async-executor-");
+        executor.initialize();
+        return executor;
+    }
+
+    @Override
+    public AsyncUncaughtExceptionHandler getAsyncUncaughtExceptionHandler() {
+        return AsyncConfigurer.super.getAsyncUncaughtExceptionHandler();
+    }
 }
